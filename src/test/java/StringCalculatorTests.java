c79801140de3e0f21123d6e3b2cfa7b7af7cd33b
GitQueenZofia <zofia.anna.grabowska@gmail.com>
2024-03-15 12:23:16 +0100

    update

--- src/test/java/StringCalculatorTests.java
+++ src/test/java/StringCalculatorTests.java
@@ -1,19 +1,16 @@
 
 import org.example.StringCalculator;
-import org.junit.Rule;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 public class StringCalculatorTests {
     StringCalculator calculator;
 
-    @Rule
-    public ExpectedException exceptionRule = ExpectedException.none();
 
     @BeforeEach
     void setUp() {
